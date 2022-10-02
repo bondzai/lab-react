c35a775da70a74a0f19d22b0e60f0a79df347865
introbond <108642001+introbond@users.noreply.github.com>
2022-10-02 21:08:26 +0700

    create state

--- app01-form-validation/src/components/Form.js
+++ app01-form-validation/src/components/Form.js
@@ -1,27 +1,34 @@
 import "./Style.css"
+import {useState} from "react"
+
 const Form = () => {
+    const [userName,setUserName] = useState("")
+    const [email,setEmail] = useState("")
+    const [password,setPassword] = useState("")
+    const [confirmPassword,setConfirmPassword] = useState("")
+
     return (
         <div className = "container">
             <form className = "form">
                 <h2> REGISTER FORM </h2>
                 <div className = "form-control">
                     <label> USER NAME </label>
-                    <input type = "text" />
+                    <input type = "text" value = {userName} onChange = {(e) => setUserName(e.target.value)}/>
                     <small> error message </small>
                 </div>
                 <div className = "form-control">
                     <label> EMAIL </label>
-                    <input type = "text" />
+                    <input type = "text" value = {email} onChange = {(e) => setEmail(e.target.value)}/>
                     <small> error message </small>
                 </div>
                 <div className = "form-control">
                     <label> PASSWORD </label>
-                    <input type = "text" />
+                    <input type = "password" value = {password} onChange = {(e) => setPassword(e.target.value)}/>                 
                     <small> error message </small>
                 </div>
                 <div className = "form-control">
                     <label> CONFIRM PASSWORD </label>
-                    <input type = "text" />
+                    <input type = "password" value = {confirmPassword} onChange = {(e) => setConfirmPassword(e.target.value)}/>  
                     <small> error message </small>
                 </div>
                 <button type = "submit"> SUBMIT </button> 
