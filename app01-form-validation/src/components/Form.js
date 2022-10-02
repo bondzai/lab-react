e7b600a955cc8696c1acce869343714c2a3caa2b
introbond <108642001+introbond@users.noreply.github.com>
2022-10-02 19:55:38 +0700

    Form Style
    CSS : built

--- app01-form-validation/src/components/Form.js
+++ app01-form-validation/src/components/Form.js
@@ -1,7 +1,31 @@
+import "./Style.css"
 const Form = () => {
     return (
-        <div>
-            Form Component
+        <div className = "container">
+            <form className = "form">
+                <h2> REGISTER FORM </h2>
+                <div className = "form-control">
+                    <label> USER NAME </label>
+                    <input type = "text" />
+                    <small> error message </small>
+                </div>
+                <div className = "form-control">
+                    <label> EMAIL </label>
+                    <input type = "text" />
+                    <small> error message </small>
+                </div>
+                <div className = "form-control">
+                    <label> PASSWORD </label>
+                    <input type = "text" />
+                    <small> error message </small>
+                </div>
+                <div className = "form-control">
+                    <label> CONFIRM PASSWORD </label>
+                    <input type = "text" />
+                    <small> error message </small>
+                </div>
+                <button type = "submit"> SUBMIT </button> 
+            </form>
         </div>
     )
 }
