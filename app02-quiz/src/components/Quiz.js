5c96a147c0a93608d09aec0f73c2afe3933f87c6
introbond <108642001+introbond@users.noreply.github.com>
2022-10-03 10:22:10 +0700

    Get data from user

--- app02-quiz/src/components/Quiz.js
+++ app02-quiz/src/components/Quiz.js
@@ -1,7 +1,19 @@
+import {useState} from "react"
+import QuestionsData from "../data/QuestionsData";
+
 const Quiz = () => {
+    const [current,setCurrent] = useState(0)
+    const [selectedChoice,setSelectedChoice] = useState("")
     return (
         <div className = "quiz">
-            <h1> QUIZ </h1>
+            <h1> {QuestionsData[current].question} </h1>
+            <div className="choices">
+                <button onClick = {()=> setSelectedChoice("A")}>{QuestionsData[current].A}</button>
+                <button onClick = {()=> setSelectedChoice("B")}>{QuestionsData[current].B}</button>
+                <button onClick = {()=> setSelectedChoice("C")}>{QuestionsData[current].C}</button>
+                <button onClick = {()=> setSelectedChoice("D")}>{QuestionsData[current].D}</button>
+            </div>
+            <p>{`${current + 1} / ${QuestionsData.length}`}</p>
         </div>
     )
 }
