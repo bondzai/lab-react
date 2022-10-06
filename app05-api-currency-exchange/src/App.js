f7a97657195f18b60cf60c9acc2da96e739a84e7
introbond <108642001+introbond@users.noreply.github.com>
2022-10-06 15:08:22 +0700

    fetch api

--- app05-api-currency-exchange/src/App.js
+++ app05-api-currency-exchange/src/App.js
@@ -1,7 +1,15 @@
+import {useEffect} from "react"
 import './App.css';
 import Currency from './components/Currency';
 import money from "./img/money.png"
+
 function App() {
+  const url = "https://api.exchangerate-api.com/v4/latest/USD"
+  useEffect(()=> {
+    fetch(url).then(res => res.json()).then(data=>console.log(data))
+  })
+
+  fetch()
   return (
     <div>
       <img src = {money} alt = "logo" className = "img-money"/>
