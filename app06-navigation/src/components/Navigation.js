88f29e0f6cc01651d75fd05d4dca8ab23879906d
introbond <108642001+introbond@users.noreply.github.com>
2022-10-06 18:56:59 +0700

    create router & className control using state

--- app06-navigation/src/components/Navigation.js
+++ app06-navigation/src/components/Navigation.js
@@ -1,15 +1,38 @@
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
+import { FaBars, FaHome } from "react-icons/fa";
+import { useState } from "react";
 
 const Navigation = () => {
+    const [showMenu,setShowMenu] = useState(false)
+    const toggleMenu = () => { setShowMenu(!showMenu) } 
     return (
         <aside>
             <div className = "navbar">
                 <div className = "navbar-toggle">
                     <Link to = "#" className = "menu-bar">
-                        Link menu
+                        <FaBars onClick = {toggleMenu}/>
                     </Link>
                 </div>
             </div>
+            <nav className = {showMenu ? "nav-menu-active" : "nav-menu"}>
+                <ul className = "nav-menu-items">
+                    <li className = "menu-item"> 
+                        <Link to = "#">
+                            <FaHome/> <span> Home </span>
+                        </Link> 
+                    </li>
+                    <li className = "menu-item"> 
+                        <Link to = "#">
+                            <FaHome/> <span> Home </span>
+                        </Link> 
+                    </li>
+                    <li className = "menu-item"> 
+                        <Link to = "#">
+                            <FaHome/> <span> Home </span>
+                        </Link> 
+                    </li>
+                </ul>
+            </nav>
         </aside>
     )
 }
