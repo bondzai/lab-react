6f264d058dc67018e217c670bc65489dafade3d9
introbond <108642001+introbond@users.noreply.github.com>
2022-10-07 08:58:43 +0700

    create state

--- app08-crud-todolist/src/App.js
+++ app08-crud-todolist/src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const [name, setName] = useState(``)
+  const submitData = (e) => {
+    e.preventDefault()
+    console.log(name)
+  }
   return (
     <section className = "container">
       <h1> TO DO LIST </h1>
-      <form className = "form-group">
+      <form className = "form-group" onSubmit = {submitData}>
         <div className = "form-control">
-          <input type = "text" className = "input-text"/>
+          <input type = "text" 
+            className = "input-text" 
+            onChange = {(e) => setName(e.target.value)}
+            value = {name}
+          />
           <button type = "submit" className = "input-button"> + </button>
         </div>
       </form>
