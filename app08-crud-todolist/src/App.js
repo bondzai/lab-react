f5d7a641d6643de308d6ccd0ff0c745d44c8eb7c
introbond <108642001+introbond@users.noreply.github.com>
2022-10-07 08:43:08 +0700

    create style

--- app08-crud-todolist/src/App.js
+++ app08-crud-todolist/src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <section className = "container">
       <h1> TO DO LIST </h1>
-      <form className = "from-group">
-        <input type = "text" className = "input-text"/>
-        <button type = "submit" className = "input-button"> + </button>
+      <form className = "form-group">
+        <div className = "form-control">
+          <input type = "text" className = "input-text"/>
+          <button type = "submit" className = "input-button"> + </button>
+        </div>
       </form>
     </section>
   );
