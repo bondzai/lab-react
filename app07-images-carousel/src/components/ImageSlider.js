47e5006e7cb505769a8aa15bcb09dd5b82459c37
introbond <108642001+introbond@users.noreply.github.com>
2022-10-07 07:00:27 +0700

    create style & control panel

--- app07-images-carousel/src/components/ImageSlider.js
+++ app07-images-carousel/src/components/ImageSlider.js
@@ -1,19 +1,22 @@
 import ImageData from "./imageData";
+import {AiOutlineArrowLeft, AiOutlineArrowRight} from "react-icons/ai"
 
 const ImageSlider = () => {
     console.log(ImageData)
     return (
         <section className = "slider">
+            <AiOutlineArrowLeft className = "arrow-left"/>
             {ImageData.map((data,index) => {
                 return (
                     <div className = "single" key = {index}>
                         <div>
-                            <img src = {data.image} alt = {data.title}/>
+                            <img src = {data.image} alt = {data.title} className = "image"/>
                             <p>{data.title}</p>
                         </div>
                     </div>
                 )
             })}
+            <AiOutlineArrowRight className = "arrow-right"/>
         </section>
     )
 }
