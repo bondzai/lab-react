e50369c04eb91611a360d84de591b642b84e6655
introbond <108642001+introbond@users.noreply.github.com>
2022-10-07 09:14:46 +0700

    crud : insert data into array with uuid v4

--- app08-crud-todolist/src/App.js
+++ app08-crud-todolist/src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import {useState} from 'react'
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-  const [name, setName] = useState(``)
+  const [name, setName] = useState("")
+  const [list,setList] = useState([])
+
   const submitData = (e) => {
     e.preventDefault()
-    console.log(name)
+    const newTask = {
+      id : uuidv4(),
+      title : name
+    }
+    setList([...list,newTask])
+    setName("")
   }
   return (
     <section className = "container">
