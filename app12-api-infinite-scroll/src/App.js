509d7a0cd0a464699f3cb27c436d843b391641fb
introbond <108642001+introbond@users.noreply.github.com>
2022-10-10 18:41:37 +0700

    fetch data

--- app12-api-infinite-scroll/src/App.js
+++ app12-api-infinite-scroll/src/App.js
@@ -1,10 +1,33 @@
 import './App.css';
+import Photo from './components/Photo';
+import {useEffect, useState} from 'react';
 
 function App() {
+    const api_key = `hiux8ewp2Amz_S5CVMvpO5MDeisItj9q9r_TAJoQglE`
+    const api_url = `https://api.unsplash.com/photos/?client_id=${api_key}&page=1`
+    const [photos, setPhotos] = useState([])
+
+    const fetchImage = async () => {
+        const response = await fetch(api_url)
+        const data = await response.json()
+        setPhotos(data)
+    }
+
+    useEffect(() => {
+        fetchImage()
+    },[])
+
     return (
-        <div className="App">
-            <h1> Hello React </h1>
-        </div>
+        <main className = "App">
+            <h1> INFINITE SCROLL PHOTO | UNSPLASH API </h1>
+            <section className = 'photos'>
+                <div className = 'display-photo'>
+                    {photos.map((item, index) => {
+                        return <Photo key = {index} {...item}/>
+                    })}
+                </div>
+            </section>
+        </main>
     )
 }
 
---
+++ app12-api-infinite-scroll/src/components/Photo.js
@@ -0,0 +1,10 @@
+const Photo = ({alt_description, urls:{regular}}) => {
+    return (
+        <div className = "single-photo">
+            <h1> Photo Component</h1>
+            <img src = {regular} alt = {alt_description}/>
+        </div>
+    )
+}
+
+export default Photo;
