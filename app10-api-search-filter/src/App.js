6f5e6041894b039349e62e66f9e7d463dbdbc0bc
introbond <108642001+introbond@users.noreply.github.com>
2022-10-10 09:06:06 +0700

    create structure and state

--- app10-api-search-filter/src/App.js
+++ app10-api-search-filter/src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello React</h1>
-    </div>
-  );
+    const data = [
+        {id : 1, name : 'Thailand', region : 'Asia', population : 100},
+        {id : 2, name : 'Japan', region : 'Asia',population : 1000},
+        {id : 3, name : 'USA', region : 'America',population : 10000}
+    ]
+    const [country, setCountry] = useState(data)
+    return (
+        <div className = "contaioner">
+            <ul>
+                {country.map((item) => {
+                    return (
+                        <li key = {item.id}> {item.name} </li>
+                    )
+                })}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
