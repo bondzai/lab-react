3e5939a3316648b5e3870437c4204f16eb7d7b69
introbond <108642001+introbond@users.noreply.github.com>
2022-10-11 09:16:12 +0700

    debug : fetch undefined using loading state -> done

--- app14-api-weather/src/App.js
+++ app14-api-weather/src/App.js
@@ -5,6 +5,7 @@ function App() {
     const cityName = "london"
     const apiKey = "34b7a90539b7ffbcd5b122fe1a30426c"
     const [city, setCity] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
 
     useEffect(() => {
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
@@ -12,6 +13,7 @@ function App() {
         .then(res => res.json())
         .then(data => {
             setCity(data)
+            setIsLoading(true)
         })
     },[])
 
@@ -20,25 +22,27 @@ function App() {
     }
 
     return (
-        <div className = "App">
-            <section>
-                <div className = "location">
-                    <h1 className = "city"> {city.name} </h1>
-                    <p className = "state"> {city.sys.country} </p>
-                </div>
-                <div className = "card">
-                    <div className = "weather">
-                        <h1> {convertTemp(city.main.temp)} &deg;c </h1>
-                        <small> max : {convertTemp(city.main.temp_max)} &deg;c, min : {convertTemp(city.main.temp_min)} &deg;c</small>
+        (isLoading && 
+            <div className = "App">
+                <section>
+                    <div className = "location">
+                        <h1 className = "city"> {city.name} </h1>
+                        <p className = "state"> {city.sys.country} </p>
                     </div>
-                    <div className = "info">
-                        <div className = "status"> status : {city.weather[0].main} </div>
-                        <div className = "humidity"> humidity : {city.main.humidity} </div>
-                        <div className = "wind"> wind speed : {city.wind.speed} </div>
+                    <div className = "card">
+                        <div className = "weather">
+                            <h1> {convertTemp(city.main.temp)} &deg;c </h1>
+                            <small> max : {convertTemp(city.main.temp_max)} &deg;c, min : {convertTemp(city.main.temp_min)} &deg;c</small>
+                        </div>
+                        <div className = "info">
+                            <div className = "status"> status : {city.weather[0].main} </div>
+                            <div className = "humidity"> humidity : {city.main.humidity} </div>
+                            <div className = "wind"> wind speed : {city.wind.speed} </div>
+                        </div>
                     </div>
-                </div>
-            </section>
-        </div>
+                </section>
+            </div>
+        )
     )
 }
 
