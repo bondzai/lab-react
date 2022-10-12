b21d3b01a0b48fc896cee4b6bbdecf35df07348f
introbond <introbond.dev@gmail.com>
2022-10-12 14:56:40 +0700

    done

--- app08-crud-todolist/src/components/List.js
+++ app08-crud-todolist/src/components/List.js
@@ -1,10 +1,13 @@
+import { BiEdit, BiTrash } from "react-icons/bi";
+
+
 const List = ({id, title, removeItem, editItem}) => {
     return (
         <div className = "list-item">
             <p className = "title"> {title} </p>
             <div className = "button-container">
-                <button onClick={() => editItem(id)}> update </button>     
-                <button onClick={() => removeItem(id)}> delete </button>     
+                <BiEdit onClick={() => editItem(id)} className = "button"/>
+                <BiTrash onClick={() => removeItem(id)} className = "button"/>  
             </div>
         </div>
     )
