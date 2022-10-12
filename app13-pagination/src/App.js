d374838ec4720e8fb44442633971a3ed5a70db13
introbond <introbond.dev@gmail.com>
2022-10-12 10:27:37 +0700

    create state

--- app13-pagination/src/App.js
+++ app13-pagination/src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 import Food from './components/Food'
+import { useState } from 'react'
+import MenuData from './data/MenuData'
+
 function App() {
+    const [foodData, setFoodData] = useState(MenuData)
+
       return (
         <div className="App">
             <Food/>
